282315f85132407dc8ce8d330513c38edb89e91b
sainiteshGit <nitesh_4u@yahoo.com>
2022-10-06 23:24:13 -0400

    check point

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,48 +1,46 @@
 import axios from 'axios';
 import * as esbuild from 'esbuild-wasm';
+import localforage from 'localforage';
+
+
+const fileCache=localforage.createInstance({
+  name:'filecache'
+});
+
+
+// (async()=>{
+  
+//   await fileCache.setItem('color','red');
+//   const color = await fileCache.getItem('color');
+//   console.log(color);
+// })
  
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if(args.path==='index.js'){
-            return { path: args.path, namespace: 'a' };
-        }        
-        if(args.path.includes('./') || args.path.includes('../')){
-          return {
-            namespace: 'a',
-            path: new URL(args.path,'https://unpkg.com'+args.resolveDir+'/').href
-          }
-        }
+      //Handle root path
+      build.onResolve({ filter: /(^index\.js$)/},()=>{
+        return {path:'index.js',namespace:'a'};
+      });
+
+      //Handle relative path in module
+      build.onResolve({filter: /^\.+\//},(args:any)=>{
+        return {
+          namespace: 'a',
+          path: new URL(args.path,'https://unpkg.com'+args.resolveDir+'/').href,
+        };
+      });
+
+      //Handle main file of a module
+      build.onResolve({ filter: /.*/ }, async (args: any) => {    
         return {
             path:`https://unpkg.com/${args.path}`,
             namespace:'a',
-        }
+        }       
       });
  
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
- 
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: `
-              import React,{useState} from 'react@16.0.0';
-              console.log(react,useState);
-            `,
-          };
-        }
 
-        const {data,request} = await axios.get(args.path);
-        console.log(request);
-        return {
-            loader:'jsx',
-            contents: data,
-            resolveDir: new URL('./',request.responseURL).pathname
-        }
-      });
     },
   };
 };
